b5bbe9cd74f711afd8ebc8af2431eaf9287107ea
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-02-14 03:03:03 +0100

    next new

---
+++ components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./SideBar.module.css"
+
+const Sidebar = () => {
+  return (
+    <div className={styles.body}>
+      <ul>
+        <div># Channel1</div>
+      </ul>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ layouts/SidebarLayout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+import Sidebar from '@/components/Sidebar';
+
+export default function Layout({children}: {children: ReactNode}){
+  return(
+    <div style={{display: "flex"}}>
+      <Sidebar></Sidebar>
+      {children}
+    </div>
+  )
+}
