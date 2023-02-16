028229c4f2259932ee7400ba9fe97366492af56e
Hiroto Yoshimori <hiroto.y.129@gmail.com>
2023-02-16 06:36:11 +0100

    material ui added

--- components/MessageTimeline.tsx
+++ components/MessageTimeline.tsx
@@ -4,6 +4,9 @@ import styles from "./MessageTimeline.module.css"
 const MessageTimeline = () => {
   return (
     <div className={styles.body}>
+      <div className={styles.message__top}>
+        <p># Channel 1</p>
+      </div>
       <div className={styles.message}>
         This is TimeLine
       </div>
--- components/SendMessageBox.tsx
+++ components/SendMessageBox.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import styles from "./SendMessageBox.module.css"
+import SendIcon from "@mui/icons-material/Send";
+
+import { Button, Input } from "@mui/material";
 
 const SendMessageBox = () => {
   return (
     <div className={styles.body}>
-      <div className={styles.body__message}>
-        This is message box
+      <div>
+        <div className={styles.body__send__message}>
+          {/* <span>This is message box</span> */}
+          <input className={styles.body__send__message__input}/>
+          <button className={styles.no__hover}>< SendIcon/></button>
+        </div>
       </div>
     </div>
   )
